ad1cd892707764e6d6717bdce0b2a778e33f0ee0
kylek <27875726+kylekinnear@users.noreply.github.com>
2019-02-12 09:59:12 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let tracker = 0
 
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[tracker]) {
+      tracker ++;
+
+      if (tracker === code.length) {
+        alert("Hurray!");
+        tracker = 0;
+      }
+    }
+    else { index = 0 }
+  })
 }
+
+init();
